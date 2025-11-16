75c432aaff62b8acde28ba41cfb1c88bcb327b28
Monideepdoley <168443234+Monideepdoley@users.noreply.github.com>
2025-11-16 22:11:04 +0530

    Create user.sql

---
+++ user.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES
+('Amit', 'amit@gmail.com'),
+('Reeta', 'reeta@gmail.com'),
+('Karan', 'karan@gmail.com'),
+('Sumi', 'sumi@gmail.com'),
+('Raj', 'raj@gmail.com'),
+('Pooja', 'pooja@gmail.com'),
+('Biki', 'biki@gmail.com');
